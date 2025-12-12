727c9d490368d4a9238ca327ac2294cbc08f40b4
Nithin <drnithin2000@gmail.com>
2025-12-12 20:12:41 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/nkoder/SpringEcom/SpringEcomApplication.java
@@ -0,0 +1,15 @@
+package com.nkoder.SpringEcom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEcomApplication {
+
+	public static void main(String[] args) {
+
+        SpringApplication.run(SpringEcomApplication.class, args);
+        System.out.println("Hello World");
+	}
+
+}
